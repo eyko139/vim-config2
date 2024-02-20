b6abf30bdd4577a76e8d0911fbc6914be28a2944
Lukas Kühne <lukas.kuehne@prodyna.com>
2024-02-20 09:03:28 +0100

    add mappings for live grep with args

--- lua/luk/plugins/telescope-live-grep-args.lua
+++ lua/luk/plugins/telescope-live-grep-args.lua
@@ -9,6 +9,29 @@ return {
 		},
 	},
 	config = function()
+		local telescope = require("telescope")
+		local lga_actions = require("telescope-live-grep-args.actions")
+		local live_grep_args_shortcuts = require("telescope-live-grep-args.shortcuts")
+		vim.keymap.set("n", "fs", live_grep_args_shortcuts.grep_word_under_cursor)
+
+		telescope.setup({
+			extensions = {
+				live_grep_args = {
+					auto_quoting = true, -- enable/disable auto-quoting
+					-- define mappings, e.g.
+					mappings = { -- extend mappings
+						i = {
+							["<C-k>"] = lga_actions.quote_prompt(),
+							["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+						},
+					},
+					-- ... also accepts theme settings, for example:
+					-- theme = "dropdown", -- use dropdown theme
+					-- theme = { }, -- use own theme spec
+					-- layout_config = { mirror=true }, -- mirror preview pane
+				},
+			},
+		})
 		require("telescope").load_extension("live_grep_args")
 	end,
 }
