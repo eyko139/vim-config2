8407c33bfa75901e57603905eb03f97092462551
Lukas Kühne <lukas.kuehne@prodyna.com>
2024-02-28 14:33:19 +0100

    Add custom format fuction (not on save)

--- lua/luk/remap.lua
+++ lua/luk/remap.lua
@@ -72,6 +72,8 @@ vim.opt.incsearch = true
 
 vim.opt.scrolloff = 8
 vim.opt.updatetime = 50
+-- format
+vim.keymap.set("n", "<leader>f", "<cmd>Format<CR>")
 
 -- move multiple lines at once
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
